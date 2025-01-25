2e114b3f9c1df5641809fcad003bf1842dd4369a
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-01-25 17:35:38 +0100

    feature: fixed tests

--- frontend/src/app/header/components/nav-item/nav-item.component.spec.ts
+++ frontend/src/app/header/components/nav-item/nav-item.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-
-import { NavItemComponent } from './nav-item.component'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
+import {NavItemComponent} from './nav-item.component'
+import {provideRouter, RouterLinkActive} from '@angular/router'
 
 describe('NavItemComponent', () => {
   let component: NavItemComponent
@@ -8,7 +8,8 @@ describe('NavItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavItemComponent]
+      imports: [NavItemComponent, RouterLinkActive],
+      providers: [provideRouter([])]
     })
     .compileComponents()
 
--- frontend/src/app/header/header.component.spec.ts
+++ frontend/src/app/header/header.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-
-import { HeaderComponent } from './header.component'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
+import {HeaderComponent} from './header.component'
+import {NavItemComponent} from './components/nav-item/nav-item.component'
+import {UserMenuComponent} from './components/user-menu/user-menu.component'
+import {provideRouter} from '@angular/router'
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent
@@ -8,9 +10,10 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [HeaderComponent, NavItemComponent, UserMenuComponent],
+      providers: [provideRouter([])]
     })
-    .compileComponents()
+      .compileComponents()
 
     fixture = TestBed.createComponent(HeaderComponent)
     component = fixture.componentInstance

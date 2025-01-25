95e161e1ca20068ffe4019ed8ab29d5ffc49eea4
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-01-25 17:57:23 +0100

    feature: header and nav-item tests

--- frontend/src/app/header/components/nav-item/nav-item.component.spec.ts
+++ frontend/src/app/header/components/nav-item/nav-item.component.spec.ts
@@ -9,9 +9,9 @@ describe('NavItemComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavItemComponent, RouterLinkActive],
-      providers: [provideRouter([])]
+      providers: [provideRouter([])],
     })
-    .compileComponents()
+      .compileComponents()
 
     fixture = TestBed.createComponent(NavItemComponent)
     component = fixture.componentInstance
@@ -20,5 +20,50 @@ describe('NavItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should default icon to null', () => {
+    expect(component.icon()).toBeNull()
+  })
+
+  it('should default displayName to null', () => {
+    expect(component.displayName()).toBeNull()
+  })
+
+  it('should default displayName to /', () => {
+    expect(component.route()).toBe('/')
+  })
+
+  it('should display green bar on active link', () => {
+    const compiled = fixture.nativeElement as HTMLElement
+    const item = compiled.querySelector('.nav-item')
+    expect(item).toBeTruthy()
+    if (!item) return
+    item.classList.add('active')
+
+    expect(getComputedStyle(item, ':after').width.slice(0, -2)).toBeGreaterThan(0)
+  })
+
+  it('should fill icon on active link', () => {
+    fixture.componentRef.setInput('icon', 'home')
+    fixture.detectChanges()
+
+    const compiled = fixture.nativeElement as HTMLElement
+    const item = compiled.querySelector('.nav-item')
+    const icon = compiled.querySelector('.icon')
+    expect(item).toBeTruthy()
+    expect(icon).toBeTruthy()
+    if (!item || !icon) return
+    item.classList.add('active')
+
+    expect(getComputedStyle(icon).fontVariationSettings).toContain('"FILL" 1')
+  })
+
+  it('should display displayName', () => {
+    fixture.componentRef.setInput('displayName', 'Home')
+    fixture.detectChanges()
+
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.textContent).toContain('Home')
   })
 })

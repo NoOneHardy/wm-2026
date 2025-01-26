34ed66f178ddd1364cf74a871b62ec96d6e3bd96
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-01-26 14:03:03 +0100

    feature: mock user

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import {Component} from '@angular/core'
+import {RouterOutlet} from '@angular/router'
 import {HeaderComponent} from './header/header.component'
+import {User} from './model/user/user'
 
 @Component({
   selector: 'wm-root',
@@ -10,4 +11,16 @@ import {HeaderComponent} from './header/header.component'
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  mockUser: User = {
+    id: 0,
+    username: 'NoOneHardy',
+    email: 'silas.hardegger@outlook.com',
+    firstname: 'Silas',
+    lastname: 'Hardegger',
+    createdAt: new Date(),
+    isActive: true,
+    points: 0,
+    updatedAt: new Date(),
+    confirmedAt: new Date()
+  }
 }

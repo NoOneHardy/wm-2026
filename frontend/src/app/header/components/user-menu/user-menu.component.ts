4fafaf709d4d92ff174fcfb0a6bfcdd336777727
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-01-29 21:51:57 +0100

    feature: basic user dropdown

--- frontend/src/app/header/components/user-menu/user-menu.component.ts
+++ frontend/src/app/header/components/user-menu/user-menu.component.ts
@@ -1,13 +1,29 @@
-import {Component, input} from '@angular/core'
+import {Component, input, signal} from '@angular/core'
 import {User} from '../../../model/user/user'
+import {RouterLink} from '@angular/router'
+import {NgIf} from '@angular/common'
 
 @Component({
   selector: 'wm-user-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgIf
+  ],
   templateUrl: './user-menu.component.html',
   styleUrl: './user-menu.component.css'
 })
 export class UserMenuComponent {
   user = input<User | null>()
+  hasNotifications = false
+
+  isDropdownExpanded = signal(false)
+
+  toggleDropdown(): void {
+    this.isDropdownExpanded.update(isExpanded => !isExpanded)
+  }
+
+  logout(): void {
+    //
+  }
 }

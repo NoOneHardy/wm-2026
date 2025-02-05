1d536b6c13a27350ed55ba373e4d358d0534e4c8
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-02-05 21:05:05 +0100

    feature: basic form field

--- frontend/src/app/shared/components/form-field/form-field.component.ts
+++ frontend/src/app/shared/components/form-field/form-field.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import {AfterViewInit, Component, ElementRef, inject} from '@angular/core'
 
 @Component({
   selector: 'wm-form-field',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core'
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.css'
 })
-export class FormFieldComponent {
+export class FormFieldComponent implements AfterViewInit {
+  private el = inject(ElementRef)
 
+  ngAfterViewInit() {
+    const input: HTMLInputElement | null = this.el.nativeElement.querySelector('input')
+
+    if (input) {
+      input.placeholder = ''
+    }
+  }
 }

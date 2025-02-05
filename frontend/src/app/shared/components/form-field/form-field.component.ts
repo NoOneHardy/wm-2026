932765609924f809b7d1b299422f19419f489a07
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-02-05 20:01:38 +0100

    feature: material components

---
+++ frontend/src/app/shared/components/form-field/form-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'wm-form-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-field.component.html',
+  styleUrl: './form-field.component.css'
+})
+export class FormFieldComponent {
+
+}
---
+++ frontend/src/app/shared/material-api.ts
@@ -0,0 +1,2 @@
+export * from './components/header/header.component'
+export * from './components/form-field/form-field.component'

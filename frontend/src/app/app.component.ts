5d58b3ee47cc5f4f5c7d4cf8405afb62980ddd9f
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-02-05 21:05:05 +0100

    feature: basic form field

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
-import {HeaderComponent} from './shared/material-api'
+import {FormFieldComponent, HeaderComponent} from './shared/material-api'
 import {User} from './model/user/user'
 
 @Component({
   selector: 'wm-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FormFieldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

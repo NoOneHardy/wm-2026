a195116989804d9ac3acacf942a665361c189660
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-11 21:36:47 +0100

    ci: fix tests

--- frontend/src/app/shared/components/header/components/user-menu/user-menu.component.spec.ts
+++ frontend/src/app/shared/components/header/components/user-menu/user-menu.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { UserMenuComponent } from './user-menu.component'
-import {User} from '../../../model/user/user'
+import {User} from '../../../../../model/user/user'
 
 describe('UserMenuComponent', () => {
   let component: UserMenuComponent

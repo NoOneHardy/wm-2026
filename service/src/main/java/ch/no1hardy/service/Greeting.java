eee4ac72e0666a49aff5a9b4fa92ffcdfb2761b5
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-11 20:49:51 +0100

    refactor: rename backend to service

--- service/src/main/java/ch/no1hardy/service/Greeting.java
+++ service/src/main/java/ch/no1hardy/service/Greeting.java
@@ -1,4 +1,4 @@
-package ch.no1hardy.backend;
+package ch.no1hardy.service;
 
 public record Greeting(Long id, String content) {
 }
--- service/src/test/java/ch/no1hardy/service/GreetingControllerTest.java
+++ service/src/test/java/ch/no1hardy/service/GreetingControllerTest.java
@@ -1,4 +1,4 @@
-package ch.no1hardy.backend;
+package ch.no1hardy.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 

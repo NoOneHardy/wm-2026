7efe68c9a256525e5882f713c1f0574b773e90b5
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-12 20:58:58 +0100

    feature: password field

--- frontend/src/app/shared/components/form-field/form-field.component.ts
+++ frontend/src/app/shared/components/form-field/form-field.component.ts
@@ -1,20 +1,35 @@
 import {AfterViewInit, Component, ElementRef, inject} from '@angular/core'
+import {NgIf} from '@angular/common'
 
 @Component({
   selector: 'wm-form-field',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './form-field.component.html',
   styleUrl: './form-field.component.css'
 })
 export class FormFieldComponent implements AfterViewInit {
   private el = inject(ElementRef)
+  private input: HTMLInputElement | null = null
+
+  isPassword = false
+  isPasswordVisible = false
 
   ngAfterViewInit() {
-    const input: HTMLInputElement | null = this.el.nativeElement.querySelector('input')
+    this.input = this.el.nativeElement.querySelector('input')
 
-    if (input) {
-      input.placeholder = ''
+    if (this.input) {
+      this.input.placeholder = ''
+      this.isPassword = this.input.type === 'password'
+    }
+  }
+
+  togglePasswordVisibility() {
+    if (this.input) {
+      this.isPasswordVisible = !this.isPasswordVisible
+      this.input.type = this.isPasswordVisible ? 'text' : 'password'
     }
   }
 }

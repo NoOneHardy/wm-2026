9b19fffc83e3675cdc81ef472e19260b25ed425b
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-14 20:40:31 +0100

    test: component testing with host component

--- frontend/src/app/shared/components/form-field/form-field.component.spec.ts
+++ frontend/src/app/shared/components/form-field/form-field.component.spec.ts
@@ -1,23 +1,34 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing'
+import {ComponentFixture} from '@angular/core/testing'
 
 import {FormFieldComponent} from './form-field.component'
+import {configureTestHost, TestHostComponent} from '../test-host/test-host.component'
 
 describe('FormFieldComponent', () => {
-  let component: FormFieldComponent
-  let fixture: ComponentFixture<FormFieldComponent>
+  let component: TestHostComponent
+  let fixture: ComponentFixture<TestHostComponent>
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FormFieldComponent]
+    [fixture, component] = await configureTestHost({
+      imports: [FormFieldComponent],
+      template: '<wm-form-field><input></wm-form-field>'
     })
-      .compileComponents()
-
-    fixture = TestBed.createComponent(FormFieldComponent)
-    component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should set placeholder to ""', () => {
+    const input = fixture.nativeElement.querySelector('input')
+    expect(input.placeholder).toBe('')
+  })
+
+  it('should set isPassword to true', async () => {
+    [fixture, component] = await configureTestHost({
+      imports: [FormFieldComponent],
+      template: '<wm-form-field><input type="password"></wm-form-field>'
+    })
+
+    expect(fixture.nativeElement.querySelector('button.show-password-icon')).toBeTruthy()
   })
 })
---
+++ frontend/src/app/shared/components/test-host/test-host.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
+
+@Component({
+  standalone: true,
+  imports: [],
+  template: ''
+})
+export class TestHostComponent {
+
+}
+
+export async function configureTestHost(config: Partial<Component>): Promise<[
+  ComponentFixture<TestHostComponent>,
+  TestHostComponent
+]> {
+  TestBed.resetTestingModule()
+  await TestBed.overrideComponent(TestHostComponent, {
+    set: config
+  }).configureTestingModule({
+    imports: [TestHostComponent]
+  }).compileComponents()
+
+  const fixture: ComponentFixture<TestHostComponent> = TestBed.createComponent(TestHostComponent)
+  const component = fixture.componentInstance
+  fixture.detectChanges()
+  await fixture.whenRenderingDone()
+  fixture.detectChanges()
+  return [fixture, component]
+}

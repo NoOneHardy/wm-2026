d570a70ef461d505bd385d6d535c1aaf5dd23cbd
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-16 21:32:17 +0100

    feature: check user infos

--- service/src/main/java/ch/no1hardy/service/model/user/UserRepository.java
+++ service/src/main/java/ch/no1hardy/service/model/user/UserRepository.java
@@ -2,5 +2,10 @@ package ch.no1hardy.service.model.user;
 
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends ListCrudRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }

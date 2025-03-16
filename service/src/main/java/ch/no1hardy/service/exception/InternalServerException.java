88aeecdc7058bce71351540557debad19e32f51a
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-16 21:44:59 +0100

    feature: validate new user

--- service/src/main/java/ch/no1hardy/service/exception/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package ch.no1hardy.service.exception;
-
-public class InternalServerException extends RuntimeException {
-    public InternalServerException(String message) {
-        super(message);
-    }
-}

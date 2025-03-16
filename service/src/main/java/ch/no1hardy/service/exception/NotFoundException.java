0f293dc4b8dbfad519e435ad6052ac8dbcf35fdd
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-16 20:53:43 +0100

    feature: error handler

---
+++ service/src/main/java/ch/no1hardy/service/exception/NotFoundException.java
@@ -0,0 +1,19 @@
+package ch.no1hardy.service.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Data
+@ResponseBody
+@ResponseStatus(HttpStatus.NOT_FOUND)
+@EqualsAndHashCode(callSuper = false)
+public class NotFoundException extends RuntimeException {
+    private final HttpStatus status = HttpStatus.NOT_FOUND;
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}

a9caf9077cb1cc02d6bc882fd6d7a1c6d41513c0
NoOneHardy <silas.hardegger@outlook.com>
2025-03-20 08:59:26 +0100

    feature: jwt configuration

--- service/src/main/java/ch/no1hardy/service/model/user/User.java
+++ service/src/main/java/ch/no1hardy/service/model/user/User.java
@@ -3,17 +3,22 @@ package ch.no1hardy.service.model.user;
 import ch.no1hardy.service.model.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "wm-user")
 @EqualsAndHashCode(callSuper = false)
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
     @NotNull
     private String username;
 
@@ -40,4 +45,30 @@ public class User extends BaseEntity {
     private String avatarUrl;
 
     private LocalDateTime confirmedAt;
+
+    @Override
+    @Transient
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }

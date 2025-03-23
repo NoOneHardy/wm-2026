677067256cf6db37ee0f65e92fd1636afeb41da1
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-23 13:06:19 +0100

    feature: signup page

--- frontend/src/app/user-management/signup/signup.component.ts
+++ frontend/src/app/user-management/signup/signup.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core'
+import {Component, inject} from '@angular/core'
 import {UserManagementPanelComponent} from '../user-management-panel/user-management-panel.component'
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms'
+import {FormFieldComponent} from '../../shared/components/form-field/form-field.component'
+import {ButtonComponent} from '../../shared/components/button/button.component'
 
 @Component({
   selector: 'wm-signup',
   standalone: true,
   imports: [
-    UserManagementPanelComponent
+    UserManagementPanelComponent,
+    ReactiveFormsModule,
+    FormFieldComponent,
+    ButtonComponent
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  private fb = inject(FormBuilder)
 
+  formGroup = this.fb.group({
+    username: this.fb.control<string>(''),
+    email: this.fb.control<string>(''),
+    firstname: this.fb.control<string>(''),
+    lastname: this.fb.control<string>(''),
+    password: this.fb.group({
+      password: this.fb.control<string>(''),
+      confirmPassword: this.fb.control<string>('')
+    })
+  })
 }

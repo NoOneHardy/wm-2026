5560cdad79f7102db4c11083a47783a80c94050a
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-27 21:01:14 +0100

    feature: basic login form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import {Routes} from '@angular/router'
 import {SignupComponent} from './user-management/signup/signup.component'
+import {LoginComponent} from './user-management/login/login.component'
 
 export const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ]

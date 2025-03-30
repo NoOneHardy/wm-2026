6a4ffdcb0ffb73e48bee4f4c7d71287325885fd4
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-30 18:55:45 +0200

    feature: snackbar service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,28 @@
 import {Component} from '@angular/core'
 import {RouterOutlet} from '@angular/router'
 import {HeaderComponent} from './shared/material-api'
-import {User} from './model/user/user'
+import {
+  SnackbarDisplayComponent
+} from './shared/components/snackbar/components/snackbar-display/snackbar-display.component'
+import {SnackbarService} from './shared/services/snackbar.service'
 
 @Component({
   selector: 'wm-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SnackbarDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  mockUser: User = {
-    id: 'asdfasdfas',
-    username: 'NoOneHardy',
-    email: 'silas.hardegger@outlook.com',
-    firstname: 'Silas',
-    lastname: 'Hardegger',
-    createdAt: new Date(),
-    isActive: true,
-    points: 0,
-    updatedAt: new Date(),
-    confirmedAt: new Date()
+  constructor(snackbarService: SnackbarService) {
+    snackbarService.addMessage({
+        message: 'User created successfully',
+      })
+    snackbarService.addMessage({
+      message: 'Could not create user: Username is already taken',
+      type: 'error',
+      duration: 10000
+    })
   }
+
 }

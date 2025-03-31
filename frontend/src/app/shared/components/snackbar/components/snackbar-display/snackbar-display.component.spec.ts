5b19e2dbf9eeb02995fbac89e7635352b322aea5
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-03-31 21:17:43 +0200

    test: snackbar display tests

--- frontend/src/app/shared/components/snackbar/components/snackbar-display/snackbar-display.component.spec.ts
+++ frontend/src/app/shared/components/snackbar/components/snackbar-display/snackbar-display.component.spec.ts
@@ -1,7 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 
-import { SnackbarDisplayComponent } from './snackbar-display.component'
+import {SnackbarDisplayComponent} from './snackbar-display.component'
 import {provideMockStore} from '@ngrx/store/testing'
+import {SnackbarService} from '../../../../services/snackbar.service'
 
 describe('SnackbarDisplayComponent', () => {
   let component: SnackbarDisplayComponent
@@ -11,8 +12,7 @@ describe('SnackbarDisplayComponent', () => {
     await TestBed.configureTestingModule({
       imports: [SnackbarDisplayComponent],
       providers: [provideMockStore()]
-    })
-    .compileComponents()
+    }).compileComponents()
 
     fixture = TestBed.createComponent(SnackbarDisplayComponent)
     component = fixture.componentInstance
@@ -21,5 +21,42 @@ describe('SnackbarDisplayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should take signal from snackbar service', () => {
+    const service = TestBed.inject(SnackbarService)
+    expect(component.messages).toEqual(service.messages)
+    expect(component.messages()).toEqual([])
+
+    service.addMessage({message: 'test'})
+    fixture.detectChanges()
+    expect(component.messages()).toEqual([{message: 'test'}])
+  })
+
+  it('should display message', () => {
+    const service = TestBed.inject(SnackbarService)
+    service.addMessage({message: 'test'})
+    fixture.detectChanges()
+
+    const snackbar = fixture.nativeElement.querySelector('wm-snackbar')
+    expect(snackbar).toBeTruthy()
+    expect(snackbar.innerText).toContain('test')
+  })
+
+  it('should display multiple messages', () => {
+    const service = TestBed.inject(SnackbarService)
+    service.addMessage({message: 'test'})
+    fixture.detectChanges()
+
+    const snackbar = fixture.nativeElement.querySelector('wm-snackbar')
+    expect(snackbar).toBeTruthy()
+    expect(snackbar.innerText).toContain('test')
+
+    service.addMessage({message: 'test2'})
+    fixture.detectChanges()
+
+    const snackbars = fixture.nativeElement.querySelectorAll('wm-snackbar')
+    expect(snackbars.length).toEqual(2)
+    expect(snackbars[1].innerText).toContain('test2')
   })
 })

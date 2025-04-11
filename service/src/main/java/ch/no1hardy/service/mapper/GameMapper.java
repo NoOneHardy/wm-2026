4760df120f6084057c8013bc792d379202afb014
NoOneHardy <silas.hardegger@outlook.com>
2025-04-11 17:31:13 +0200

    feature: map score and bets using

--- service/src/main/java/ch/no1hardy/service/mapper/GameMapper.java
+++ service/src/main/java/ch/no1hardy/service/mapper/GameMapper.java
@@ -9,20 +9,26 @@ import ch.no1hardy.service.model.game.Game;
 import ch.no1hardy.service.model.game.Score;
 import ch.no1hardy.service.model.group.GroupRepository;
 import ch.no1hardy.service.model.team.TeamRepository;
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
-        uses = {TeamRepository.class, GroupRepository.class, TeamMapper.class}
+        uses = {TeamRepository.class, GroupRepository.class, TeamMapper.class, UserHelper.class}
 )
 public interface GameMapper extends EntityMapper<Game, GameReq, BetGameRes> {
     BetRes toDto(Bet entity);
 
+    @Mapping(target = "bet", source = "bets", qualifiedByName = "getUserBet")
+    BetGameRes toDto(Game game);
+
+    @Mapping(target = "gameId", source = "game", qualifiedByName = "getGameId")
     ScoreRes toDto(Score entity);
+
+    @Named("getGameId")
+    default String getId(Game game) {
+        return game.getId();
+    }
 }

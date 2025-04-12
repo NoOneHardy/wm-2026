2e2705457453a8b2486749aadd83071b90c4d020
NoOneHardy <silas.hardegger@outlook.com>
2025-04-12 12:02:04 +0200

    feature: calculate percentage and lastSavedAt

--- service/src/main/java/ch/no1hardy/service/mapper/GroupMapper.java
+++ service/src/main/java/ch/no1hardy/service/mapper/GroupMapper.java
@@ -2,17 +2,43 @@ package ch.no1hardy.service.mapper;
 
 import ch.no1hardy.service.front.group.GroupReq;
 import ch.no1hardy.service.front.group.GroupRes;
+import ch.no1hardy.service.model.game.Game;
+import ch.no1hardy.service.model.game.Score;
 import ch.no1hardy.service.model.group.Group;
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        uses = {GameMapper.class, UserHelper.class}
 )
 public interface GroupMapper extends EntityMapper<Group, GroupReq, GroupRes> {
+    @Mapping(target = "percentage", source = "games", qualifiedByName = "getPercentage")
+    @Mapping(target = "lastSavedAt", source = "games", qualifiedByName = "getLastSavedAt")
+    @Mapping(target = "percentageResult", source = "games", qualifiedByName = "getPercentageResult")
+    @Mapping(target = "lastSavedAtResult", source = "games", qualifiedByName = "getLastSavedAtResult")
+    GroupRes toDto(Group entity);
+
+    @Named("getPercentageResult")
+    default Double getPercentageResult(List<Game> games) {
+        double betCount = games.stream().map(Game::getResult).filter(Objects::nonNull).toList().size();
+        double gameCount = games.size();
+        return gameCount == 0 ? 0.0 : betCount / gameCount * 100;
+    }
+
+    @Named("getLastSavedAtResult")
+    default LocalDateTime getLastSavedAtResult(List<Game> games) {
+        List<Score> scores = games.stream().map(Game::getResult).filter(Objects::nonNull).toList();
+        if (scores.isEmpty()) return null;
+        return scores.stream()
+                .map(Score::getUpdatedAt)
+                .max(LocalDateTime::compareTo)
+                .orElse(null);
+    }
 }
--- service/src/main/java/ch/no1hardy/service/service/GroupService.java
+++ service/src/main/java/ch/no1hardy/service/service/GroupService.java
@@ -1,7 +1,6 @@
 package ch.no1hardy.service.service;
 
 import ch.no1hardy.service.exception.NotFoundException;
-import ch.no1hardy.service.front.game.BetGameRes;
 import ch.no1hardy.service.front.game.BetReq;
 import ch.no1hardy.service.front.group.GroupReq;
 import ch.no1hardy.service.front.group.GroupRes;
@@ -23,13 +22,13 @@ public class GroupService {
 
     public GroupRes create(GroupReq dto) {
         Group group = repository.save(mapper.toEntity(dto));
-        return mapFromEntity(group);
+        return mapper.toDto(group);
     }
 
     public GroupRes getGroup(String id) {
         Group group = repository.findById(id).orElse(null);
         if (group == null) throw new NotFoundException("Group " + id + " not found");
-        return mapFromEntity(group);
+        return mapper.toDto(group);
     }
 
     public GroupRes updateBets(String id, List<BetReq> bets) {
@@ -44,22 +43,6 @@ public class GroupService {
                 gameService.uploadBet(bet.getGame(), bet);
             }
         }
-        return mapFromEntity(group);
-    }
-
-
-    // TODO: remove
-    private static GroupRes mapFromEntity(Group group) {
-        return GroupRes.builder()
-                .id(group.getId())
-                .name(group.getName())
-                .games(group.getGames().stream().map(g -> BetGameRes.builder().build()).toList())
-                .isKnockout(group.getIsKnockout())
-                .percentage(10.0)
-                .lastSavedAt(null)
-                .createdAt(group.getCreatedAt())
-                .updatedAt(group.getUpdatedAt())
-                .deletedAt(group.getDeletedAt())
-                .build();
+        return mapper.toDto(group);
     }
 }

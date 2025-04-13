92ef0fdfe8bd4213eea4266e084bdd9840911f6b
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-04-13 18:00:32 +0200

    test: fix tests

--- frontend/src/app/shared/components/overview/overview.component.spec.ts
+++ frontend/src/app/shared/components/overview/overview.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 
-import { OverviewComponent } from './overview.component'
+import {OverviewComponent} from './overview.component'
+import {provideMockStore} from '@ngrx/store/testing'
+import {selectGroups} from '../../store/tournament.feature'
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent
@@ -8,9 +10,14 @@ describe('OverviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OverviewComponent]
+      imports: [OverviewComponent],
+      providers: [provideMockStore({
+        selectors: [
+          {selector: selectGroups, value: []}
+        ]
+      })]
     })
-    .compileComponents()
+      .compileComponents()
 
     fixture = TestBed.createComponent(OverviewComponent)
     component = fixture.componentInstance
--- frontend/src/app/shared/services/group/group.service.spec.ts
+++ frontend/src/app/shared/services/group/group.service.spec.ts
@@ -1,12 +1,15 @@
-import { TestBed } from '@angular/core/testing'
+import {TestBed} from '@angular/core/testing'
 
-import { GroupService } from './group.service'
+import {GroupService} from './group.service'
+import {provideHttpClient} from '@angular/common/http'
 
 describe('GroupService', () => {
   let service: GroupService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()]
+    })
     service = TestBed.inject(GroupService)
   })
 

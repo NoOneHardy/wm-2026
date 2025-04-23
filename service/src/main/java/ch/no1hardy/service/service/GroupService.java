16fa4a5159a5265476ceeeef2a4cb989e2dd8ac1
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-04-23 20:39:09 +0200

    feature: prevent betting on past games

--- service/src/main/java/ch/no1hardy/service/service/GroupService.java
+++ service/src/main/java/ch/no1hardy/service/service/GroupService.java
@@ -41,7 +41,6 @@ public class GroupService {
 
         for (BetReq bet : bets.stream().filter(BetReq::isValid).toList()) {
             if (games.stream().map(Game::getId).toList().contains(bet.getGame())) {
-                if (bet.getScoreTeamHome() == null || bet.getScoreTeamGuest() == null) continue;
                 gameService.uploadBet(bet.getGame(), bet);
             }
         }

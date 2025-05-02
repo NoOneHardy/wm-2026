ba3796dc9b6864345ce34c1d7741906bfd316d24
NoOneHardy <silas.hardegger@outlook.com>
2025-05-02 14:37:57 +0200

    feature: add previous games to team

--- service/src/main/java/ch/no1hardy/service/mapper/TeamMapper.java
+++ service/src/main/java/ch/no1hardy/service/mapper/TeamMapper.java
@@ -1,13 +1,13 @@
 package ch.no1hardy.service.mapper;
 
+import ch.no1hardy.service.front.game.PreviousGameRes;
+import ch.no1hardy.service.front.team.ExtendedTeamRes;
 import ch.no1hardy.service.front.team.TeamReq;
 import ch.no1hardy.service.front.team.TeamRes;
+import ch.no1hardy.service.model.game.Game;
 import ch.no1hardy.service.model.team.Team;
 import ch.no1hardy.service.model.team.TeamRepository;
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
 @Mapper(
         componentModel = "spring",
@@ -17,4 +17,8 @@ import org.mapstruct.ReportingPolicy;
         uses = {TeamRepository.class}
 )
 public interface TeamMapper extends EntityMapper<Team, TeamReq, TeamRes> {
+    @Mapping(target = "previousGames", source = "previousGames")
+    ExtendedTeamRes toExtendedDto(Team entity);
+
+    PreviousGameRes toPreviousGameDto(Game game);
 }

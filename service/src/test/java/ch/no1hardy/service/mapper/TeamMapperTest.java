c4017f44e19cfc3f6b32ed258a1d6d6cf2e59cc6
NoOneHardy <silas.hardegger@outlook.com>
2025-05-08 10:54:59 +0200

    test: previous game mapping

---
+++ service/src/test/java/ch/no1hardy/service/mapper/TeamMapperTest.java
@@ -0,0 +1,52 @@
+package ch.no1hardy.service.mapper;
+
+import ch.no1hardy.service.front.game.PreviousGameRes;
+import ch.no1hardy.service.model.game.Game;
+import ch.no1hardy.service.model.game.Score;
+import ch.no1hardy.service.model.group.Group;
+import ch.no1hardy.service.model.team.Team;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TeamMapperTest {
+    @Autowired
+    private TeamMapper teamMapper;
+
+    @Test
+    void shouldMapGameToPreviousGame() {
+        Game game = new Game();
+        game.setId("game-1");
+        game.setTimestamp(LocalDateTime.of(2025, 5, 8, 20, 0));
+
+        Team homeTeam = new Team();
+        homeTeam.setId("team-home");
+        game.setTeamHome(homeTeam);
+
+        Team guestTeam = new Team();
+        guestTeam.setId("team-guest");
+        game.setTeamGuest(guestTeam);
+
+        Group group = new Group();
+        group.setId("group-1");
+        group.setName("Gruppe 1");
+        game.setGroup(group);
+
+        Score result = new Score();
+        result.setId("score-1");
+        game.setResult(result);
+
+        PreviousGameRes res = teamMapper.toPreviousGameDto(game);
+
+        assertEquals("game-1", res.getId());
+        assertEquals("team-home", res.getTeamHome().getId());
+        assertEquals("team-guest", res.getTeamGuest().getId());
+        assertEquals("score-1", res.getResult().getId());
+        assertEquals("Gruppe 1", res.getGroup());
+    }
+}

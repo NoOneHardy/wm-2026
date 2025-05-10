4ee4d7f09fc25f52b3190e17027ee8b5abd1d2b9
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-05-10 22:05:35 +0200

    test: header is admin

--- frontend/src/app/shared/components/header/header.component.spec.ts
+++ frontend/src/app/shared/components/header/header.component.spec.ts
@@ -4,26 +4,24 @@ import {NavItemComponent} from './components/nav-item/nav-item.component'
 import {UserMenuComponent} from './components/user-menu/user-menu.component'
 import {provideRouter} from '@angular/router'
 import {UserButtonComponent} from './components/user-button/user-button.component'
-import {provideMockStore} from '@ngrx/store/testing'
-import {selectUser} from '../../../user-management/store/user.feature'
+import {MockStore, provideMockStore} from '@ngrx/store/testing'
+import {selectIsAdmin, selectUser} from '../../../user-management/store/user.feature'
+import {Role} from '../../../model/user/role'
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent
   let fixture: ComponentFixture<HeaderComponent>
+  let store: MockStore
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HeaderComponent, NavItemComponent, UserMenuComponent, UserButtonComponent],
-      providers: [provideRouter([]), provideMockStore({
-        initialState: {
-          user: null
-        },
-        selectors: [
-          {selector: selectUser, value: null}
-        ]
-      })]
-    })
-      .compileComponents()
+      providers: [provideRouter([]), provideMockStore()]
+    }).compileComponents()
+
+    store = TestBed.inject(MockStore)
+    store.overrideSelector(selectUser, null)
+    store.refreshState()
 
     fixture = TestBed.createComponent(HeaderComponent)
     component = fixture.componentInstance
@@ -55,5 +53,35 @@ describe('HeaderComponent', () => {
     expect(component.user()).toBeNull()
     const compiled = fixture.nativeElement as HTMLElement
     expect(compiled.querySelector('wm-nav-item')?.textContent).toContain('Home')
+  })
+
+  it('should load user from store', () => {
+    expect(component.user()).toBeNull()
+
+    store.overrideSelector(selectUser, {
+      id: 'user-1',
+      username: 'User',
+      email: 'user@no1hardy.ch',
+      firstname: 'Silas',
+      lastname: 'No1hardy',
+      isActive: true,
+      role: Role.USER,
+      points: 0,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    store.refreshState()
+    fixture.detectChanges()
+
+    expect(component.user()?.id).toBe('user-1')
+  })
+
+  it('should load is admin from store', () => {
+    expect(component.isAdmin()).toBeFalse()
+
+    store.overrideSelector(selectIsAdmin, true)
+    store.refreshState()
+    fixture.detectChanges()
+    expect(component.isAdmin()).toBe(true)
   })
 })

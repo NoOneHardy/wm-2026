697835296b62153520929a434705d0ee45e9ddb8
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-05-13 20:50:36 +0200

    feature: result management views

--- frontend/src/app/admin/result-management/result-management.component.ts
+++ frontend/src/app/admin/result-management/result-management.component.ts
@@ -1,11 +1,20 @@
-import {Component} from '@angular/core'
+import {Component, inject} from '@angular/core'
+import {Store} from '@ngrx/store'
+import {hasActiveGroup} from '../../shared/store/tournament.feature'
+import {GroupViewComponent} from '../../shared/components/group-view/group-view.component'
+import {OverviewComponent} from '../../shared/components/overview/overview.component'
 
 @Component({
   selector: 'wm-result-management',
   standalone: true,
-  imports: [],
-  templateUrl: './result-management.component.html',
-  styleUrl: './result-management.component.css'
+  imports: [
+    GroupViewComponent,
+    OverviewComponent
+  ],
+  templateUrl: './result-management.component.html'
 })
 export class ResultManagementComponent {
+  private store = inject(Store)
+
+  hasGroupSelected = this.store.selectSignal(hasActiveGroup)
 }

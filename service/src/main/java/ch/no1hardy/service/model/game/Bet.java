53addcf2a23b8906210a0651b75775958b861f35
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-05-13 20:44:24 +0200

    refactor: bet with inheritance

--- service/src/main/java/ch/no1hardy/service/model/game/Bet.java
+++ service/src/main/java/ch/no1hardy/service/model/game/Bet.java
@@ -1,19 +1,17 @@
 package ch.no1hardy.service.model.game;
 
 import ch.no1hardy.service.front.game.BetReq;
-import ch.no1hardy.service.model.BaseEntity;
 import ch.no1hardy.service.model.user.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.ToString;
 
 import java.util.Objects;
 
 @Data
 @Entity
-public class Bet extends BaseEntity {
+public class Bet extends Score {
     @NotNull
     private Integer joker;
 
@@ -21,41 +19,9 @@ public class Bet extends BaseEntity {
     @ManyToOne
     private User user;
 
-    @NotNull
-    @ManyToOne
-    @ToString.Exclude
-    private Game game;
-
-    @NotNull
-    private Integer scoreTeamHome;
-
-    @NotNull
-    private Integer scoreTeamGuest;
-
-    public Boolean isHomeTeamWinner() {
-        return scoreTeamHome > scoreTeamGuest;
-    }
-
-    public Boolean isGuestTeamWinner() {
-        return scoreTeamGuest > scoreTeamHome;
-    }
-
-    public Boolean isTie() {
-        return scoreTeamHome.equals(scoreTeamGuest);
-    }
-
-    public Integer getTotalScore() {
-        return scoreTeamHome + scoreTeamGuest;
-    }
-
     public boolean equals(BetReq bet) {
         return Objects.equals(this.getScoreTeamGuest(), bet.getScoreTeamGuest())
                 && Objects.equals(this.getScoreTeamHome(), bet.getScoreTeamHome())
                 && Objects.equals(this.getJoker(), bet.getJoker());
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
     }
 }

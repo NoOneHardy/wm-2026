0808770108905c125d661a57620f67b17348330d
NoOneHardy <silas.hardegger@outlook.com>
2025-05-22 08:31:59 +0200

    test: position component

--- frontend/src/app/shared/components/position/position.component.spec.ts
+++ frontend/src/app/shared/components/position/position.component.spec.ts
@@ -1,23 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
 import { PositionComponent } from './position.component'
+import {Ranking} from '../../../model/leaderboard/ranking'
 
 describe('PositionComponent', () => {
   let component: PositionComponent
   let fixture: ComponentFixture<PositionComponent>
+  let ranking: Ranking
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PositionComponent]
-    })
-    .compileComponents()
+    }).compileComponents()
+
+    ranking = {
+      ranking: 1,
+      avatar: null,
+      points: 100,
+      previousRanking: 3,
+      username: 'No1Hardy'
+    }
 
     fixture = TestBed.createComponent(PositionComponent)
     component = fixture.componentInstance
+    fixture.componentRef.setInput('ranking', ranking)
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should parse highlight input', () => {
+    expect(component.highlight()).toBe(false)
+    fixture.componentRef.setInput('highlight', true)
+    fixture.detectChanges()
+    expect(component.highlight()).toBe(true)
+    fixture.componentRef.setInput('highlight', '')
+    fixture.detectChanges()
+    expect(component.highlight()).toBe(true)
   })
 })

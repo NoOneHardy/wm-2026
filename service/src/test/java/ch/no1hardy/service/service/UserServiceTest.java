c6eed2479faff7a55f8e2ec04b1598430cb62148
NoOneHardy <silas.hardegger@outlook.com>
2025-05-23 14:48:12 +0200

    test: leaderboard tests

--- service/src/test/java/ch/no1hardy/service/service/UserServiceTest.java
+++ service/src/test/java/ch/no1hardy/service/service/UserServiceTest.java
@@ -1,20 +1,29 @@
 package ch.no1hardy.service.service;
 
+import ch.no1hardy.service.front.leaderboard.RankingRes;
 import ch.no1hardy.service.model.game.Bet;
 import ch.no1hardy.service.model.game.Score;
+import ch.no1hardy.service.model.user.User;
+import ch.no1hardy.service.model.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class UserServiceTest {
     @Autowired
     private UserService service;
+
+    @MockitoBean
+    private UserRepository userRepository;
 
     @Test
     void shouldCalculatePointsForCorrectBet() {
@@ -203,5 +212,37 @@ public class UserServiceTest {
         } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    @Test
+    void shouldCalculateLeaderboard() {
+        User user1 = new User();
+        user1.setId("user-1");
+        user1.setPoints(100);
+        user1.setLastReviewedPoints(50);
+
+        User user2 = new User();
+        user2.setId("user-2");
+        user2.setPoints(200);
+        user2.setLastReviewedPoints(150);
+
+        User user3 = new User();
+        user3.setId("user-3");
+        user3.setPoints(200);
+        user3.setLastReviewedPoints(100);
+
+        User user4 = new User();
+        user4.setId("user-4");
+        user4.setPoints(150);
+        user4.setLastReviewedPoints(0);
+
+        when(userRepository.findAll()).thenReturn(List.of(user1, user2, user3, user4));
+
+        List<RankingRes> res = service.getLeaderboard();
+
+        assertEquals(user1.getId(), res.get(3).getId());
+        assertEquals(user2.getId(), res.get(0).getId());
+        assertEquals(user3.getId(), res.get(1).getId());
+        assertEquals(user4.getId(), res.get(2).getId());
     }
 }

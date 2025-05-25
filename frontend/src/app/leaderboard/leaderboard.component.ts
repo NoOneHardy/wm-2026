c5591cc9b6107b9c658d28c2ed5f73d03f76b77b
NoOneHardy <silas.hardegger@outlook.com>
2025-05-25 08:25:18 +0200

    feature: leaderboard routing

---
+++ frontend/src/app/leaderboard/leaderboard.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'wm-leaderboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './leaderboard.component.html',
+  styleUrl: './leaderboard.component.css'
+})
+export class LeaderboardComponent {
+}

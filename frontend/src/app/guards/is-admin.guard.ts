4582e1aa387e4f1ca6cd15bed9c813d83e9883b5
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-05-28 22:08:16 +0200

    feature: guards wait for user data

--- frontend/src/app/guards/is-admin.guard.ts
+++ frontend/src/app/guards/is-admin.guard.ts
@@ -1,13 +1,20 @@
 import {CanActivateFn, Router} from '@angular/router'
 import {inject} from '@angular/core'
 import {Store} from '@ngrx/store'
-import {selectIsAdmin} from '../user-management/store/user.feature'
+import {selectIsAdmin, selectIsUserLoading} from '../user-management/store/user.feature'
+import {combineLatest, filter, map, Observable} from 'rxjs'
 
 export const isAdminGuard: CanActivateFn = () => {
   const store = inject(Store)
   const router = inject(Router)
 
-  const isAdmin = store.selectSignal(selectIsAdmin)
+  const isAdmin$: Observable<boolean> = store.select(selectIsAdmin)
+  const isLoading$: Observable<boolean> = store.select(selectIsUserLoading)
 
-  return isAdmin() ? true : router.createUrlTree(['/'])
+  return combineLatest([isAdmin$, isLoading$]).pipe(
+    filter(([, isLoading]) => !isLoading),
+    map(([isAdmin]) => {
+      return isAdmin ? true : router.createUrlTree(['/'])
+    })
+  )
 }

50450253408d98d5d02e9de59e5800e6220241f8
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-05-28 23:06:31 +0200

    feature: leaderboard user position

--- frontend/src/app/leaderboard/leaderboard.component.spec.ts
+++ frontend/src/app/leaderboard/leaderboard.component.spec.ts
@@ -1,11 +1,13 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing'
 
 import {LeaderboardComponent} from './leaderboard.component'
-import {provideMockStore} from '@ngrx/store/testing'
+import {MockStore, provideMockStore} from '@ngrx/store/testing'
+import {selectLeaderboard} from '../shared/store/tournament.feature'
 
 describe('LeaderboardComponent', () => {
   let component: LeaderboardComponent
   let fixture: ComponentFixture<LeaderboardComponent>
+  let store: MockStore
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +15,9 @@ describe('LeaderboardComponent', () => {
       providers: [provideMockStore()]
     }).compileComponents()
 
+    store = TestBed.inject(MockStore)
+    store.overrideSelector(selectLeaderboard, [])
+    store.refreshState()
     fixture = TestBed.createComponent(LeaderboardComponent)
     component = fixture.componentInstance
     fixture.detectChanges()

5dd8415743c9b2d0aa0a97e98e07a8e3a713a5e6
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-05-28 22:25:51 +0200

    test: update guards

--- frontend/src/app/guards/is-admin.guard.spec.ts
+++ frontend/src/app/guards/is-admin.guard.spec.ts
@@ -1,38 +1,70 @@
 import {TestBed} from '@angular/core/testing'
-import {ActivatedRouteSnapshot, CanActivateFn, provideRouter, Router, RouterStateSnapshot} from '@angular/router'
+import {ActivatedRouteSnapshot, GuardResult, provideRouter, Router, RouterStateSnapshot} from '@angular/router'
 
 import {isAdminGuard} from './is-admin.guard'
 import {MockStore, provideMockStore} from '@ngrx/store/testing'
-import {selectIsAdmin} from '../user-management/store/user.feature'
+import {selectIsAdmin, selectIsUserLoading} from '../user-management/store/user.feature'
+import {Observable} from 'rxjs'
 
 describe('isAdminGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => isAdminGuard(...guardParameters))
   const route: ActivatedRouteSnapshot = {} as ActivatedRouteSnapshot
   const state: RouterStateSnapshot = {} as RouterStateSnapshot
   let store: MockStore
+  let result: GuardResult | null
+
+  const subscribeToGuard = () => {
+    const guard$: Observable<GuardResult> = TestBed.runInInjectionContext(() => isAdminGuard(route, state)) as Observable<GuardResult>
+    guard$.subscribe((guardResult) => {
+      result = guardResult
+    })
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [provideRouter([]), provideMockStore()]
     })
 
+    result = null
     store = TestBed.inject(MockStore)
-  })
-
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy()
   })
 
   it('should return true if user is admin', () => {
     store.overrideSelector(selectIsAdmin, true)
-    const result = executeGuard(route , state)
+    store.refreshState()
+    subscribeToGuard()
+
     expect(result).toBe(true)
   })
 
   it('should return a url tree for "/" if user is not admin', () => {
     store.overrideSelector(selectIsAdmin, false)
-    const result = executeGuard(route , state)
+    store.refreshState()
+    subscribeToGuard()
     expect(result).toEqual(TestBed.inject(Router).createUrlTree(['/']))
+  })
+
+  it('should wait until loading is done and redirect', () => {
+    store.overrideSelector(selectIsAdmin, false)
+    store.overrideSelector(selectIsUserLoading, true)
+    store.refreshState()
+    subscribeToGuard()
+    expect(result).toBeNull()
+
+    store.overrideSelector(selectIsUserLoading, false)
+    store.refreshState()
+    expect(result).toEqual(TestBed.inject(Router).createUrlTree(['/']))
+  })
+
+  it('should wait until loading is done and allow', () => {
+    store.overrideSelector(selectIsAdmin, false)
+    store.overrideSelector(selectIsUserLoading, true)
+    store.refreshState()
+    subscribeToGuard()
+    expect(result).toBeNull()
+
+    store.overrideSelector(selectIsUserLoading, false)
+    store.overrideSelector(selectIsAdmin, true)
+    store.refreshState()
+    expect(result).toBeTrue()
   })
 })
--- frontend/src/app/guards/is-logged-in.guard.spec.ts
+++ frontend/src/app/guards/is-logged-in.guard.spec.ts
@@ -1,50 +1,84 @@
 import {TestBed} from '@angular/core/testing'
-import {ActivatedRouteSnapshot, CanActivateFn, provideRouter, Router, RouterStateSnapshot} from '@angular/router'
+import {ActivatedRouteSnapshot, GuardResult, provideRouter, Router, RouterStateSnapshot} from '@angular/router'
 
 import {isLoggedInGuard} from './is-logged-in.guard'
 import {MockStore, provideMockStore} from '@ngrx/store/testing'
-import {selectUser} from '../user-management/store/user.feature'
+import {selectIsUserLoading, selectUser} from '../user-management/store/user.feature'
 import {Role} from '../model/user/role'
+import {Observable} from 'rxjs'
+
+const mockUser = {
+  id: 'user-1',
+  username: 'User',
+  email: 'user@no1hardy.ch',
+  firstname: 'Silas',
+  lastname: 'No1hardy',
+  isActive: true,
+  role: Role.USER,
+  points: 0,
+  createdAt: new Date(),
+  updatedAt: new Date()
+}
 
 describe('isLoggedInGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => isLoggedInGuard(...guardParameters))
   const route: ActivatedRouteSnapshot = {} as ActivatedRouteSnapshot
   const state: RouterStateSnapshot = {} as RouterStateSnapshot
   let store: MockStore
+  let result: GuardResult | null
+
+  const subscribeToGuard = () => {
+    const guard$: Observable<GuardResult> = TestBed.runInInjectionContext(() => isLoggedInGuard(route, state)) as Observable<GuardResult>
+    guard$.subscribe((guardResult) => {
+      result = guardResult
+    })
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [provideRouter([]), provideMockStore()]
     })
 
+    result = null
     store = TestBed.inject(MockStore)
-    store.overrideSelector(selectUser, {
-      id: 'user-1',
-      username: 'User',
-      email: 'user@no1hardy.ch',
-      firstname: 'Silas',
-      lastname: 'No1hardy',
-      isActive: true,
-      role: Role.USER,
-      points: 0,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    })
-  })
-
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy()
+    store.overrideSelector(selectUser, {...mockUser})
   })
 
   it('should return true if user is logged in', () => {
-    const result = executeGuard(route , state)
-    expect(result).toBe(true)
+    subscribeToGuard()
+    expect(result).toBeTrue()
   })
 
   it('should return a url tree for "/" if user is not logged in', () => {
     store.overrideSelector(selectUser, null)
-    const result = executeGuard(route , state)
-    expect(result).toEqual(TestBed.inject(Router).createUrlTree(['/']))
+    subscribeToGuard()
+
+    expect(result).toEqual(TestBed.inject(Router).createUrlTree(['/login']))
+  })
+
+  it('should wait until loading is done and redirect', () => {
+    store.overrideSelector(selectUser, null)
+    store.overrideSelector(selectIsUserLoading, true)
+    store.refreshState()
+
+    subscribeToGuard()
+    expect(result).toBeNull()
+
+    store.overrideSelector(selectIsUserLoading, false)
+    store.refreshState()
+    expect(result).toEqual(TestBed.inject(Router).createUrlTree(['/login']))
+  })
+
+  it('should wait until loading is done and allow', () => {
+    store.overrideSelector(selectUser, null)
+    store.overrideSelector(selectIsUserLoading, true)
+    store.refreshState()
+
+    subscribeToGuard()
+    expect(result).toBeNull()
+
+    store.overrideSelector(selectIsUserLoading, false)
+    store.overrideSelector(selectUser, {...mockUser})
+    store.refreshState()
+    expect(result).toBeTrue()
   })
 })

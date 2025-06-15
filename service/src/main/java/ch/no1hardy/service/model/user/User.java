b9b63dd7e0f6fcf2685d8637718c35dfd9904899
NoOneHardy <silas.hardegger@outlook.com>
2025-06-15 09:02:13 +0200

    docs: update user javadoc

--- service/src/main/java/ch/no1hardy/service/model/user/User.java
+++ service/src/main/java/ch/no1hardy/service/model/user/User.java
@@ -20,29 +20,57 @@ import java.util.List;
 @Table(name = "wm-user")
 @EqualsAndHashCode(callSuper = false)
 public class User extends BaseEntity implements UserDetails {
+    /**
+     * The username of the user. This is used for login and must be unique.
+     */
     @NotNull
     private String username;
 
+    /**
+     * The password of the user. This is used for login and must be at least 8 characters long.
+     */
     @NotNull
     private String password;
 
+    /**
+     * The email address of the user. This is used for communication and must be unique.
+     */
     @NotNull
     private String email;
 
+    /**
+     * The first name of the user. This is used for display purposes.
+     */
     @NotNull
     private String firstname;
 
+    /**
+     * The last name of the user. This is used for display purposes.
+     */
     @NotNull
     private String lastname;
 
+    /**
+     * The amount of points the user has earned.
+     */
     @NotNull
     private Integer points = 0;
 
+    /**
+     * The role of the user. Only used for authorization
+     */
     @NotNull
     private Role role = Role.USER;
 
+    /**
+     * The points the user has earned before the last upload of results.
+     */
     private Integer lastReviewedPoints = 0;
 
+    /**
+     * The avatar URL of the user. This is used for display purposes.
+     * TODO: Implement avatar upload
+     */
     private String avatarUrl;
 
     /**
@@ -55,13 +83,22 @@ public class User extends BaseEntity implements UserDetails {
      * The date when an admin has reviewed the user and accepted or denied it
      */
     private LocalDateTime applicationReviewedAt;
-    private UserApplicationStatus userApplicationStatus;
+    /**
+     * The status of the user application. Can be either ACCEPTED, DENIED or PENDING.
+     */
+    private UserApplicationStatus userApplicationStatus = UserApplicationStatus.PENDING;
 
+    /**
+     * A list with all bets the user has placed.
+     */
     @OneToMany(mappedBy = "user")
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
     private List<Bet> bets = List.of();
 
+    /**
+     * The authorities granted to the user. This is used for authorization.
+     */
     @Override
     @Transient
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -69,26 +106,41 @@ public class User extends BaseEntity implements UserDetails {
         return List.of(authority);
     }
 
+    /**
+     * Returns whether the account has not expired.
+     */
     @Override
     public boolean isAccountNonExpired() {
         return UserDetails.super.isAccountNonExpired();
     }
 
+    /**
+     * Returns whether the account is not locked.
+     */
     @Override
     public boolean isAccountNonLocked() {
         return UserDetails.super.isAccountNonLocked();
     }
 
+    /**
+     * Returns whether the credentials (password) are not expired.
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return UserDetails.super.isCredentialsNonExpired();
     }
 
+    /**
+     * Returns whether the user is active. This is used to determine if the user can log in.
+     */
     @Override
     public boolean isEnabled() {
         return UserDetails.super.isEnabled();
     }
 
+    /**
+     * Returns whether the user has been confirmed by an admin. This is used to determine if the user should be included in the ranking.
+     */
     public boolean isConfirmed() {
         return isActive() && getApplicationReviewedAt() != null && getUserApplicationStatus() == UserApplicationStatus.ACCEPTED;
     }

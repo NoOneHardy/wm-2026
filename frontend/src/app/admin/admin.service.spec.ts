aedd2ced4f9706e098094683ee2d7ce7e1c6b218
NoOneHardy <silas.hardegger@outlook.com>
2025-06-19 15:30:03 +0200

    test: admin service

--- frontend/src/app/admin/admin.service.spec.ts
+++ frontend/src/app/admin/admin.service.spec.ts
@@ -1,19 +1,62 @@
-import { TestBed } from '@angular/core/testing'
+import {TestBed} from '@angular/core/testing'
 
-import { AdminService } from './admin.service'
+import {AdminService} from './admin.service'
+import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing'
+import {User} from '../model/user/user'
+import {UserApplicationStatus} from '../model/user/user-application-status'
+import {Role} from '../model/user/role'
 import {provideHttpClient} from '@angular/common/http'
 
 describe('AdminService', () => {
   let service: AdminService
+  let httpMock: HttpTestingController
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient()]
+      providers: [provideHttpClient(), provideHttpClientTesting()]
     })
     service = TestBed.inject(AdminService)
+    httpMock = TestBed.inject(HttpTestingController)
+  })
+
+  afterEach(() => {
+    httpMock.verify()
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should return a list of users', () => {
+    const mockUsers: User[] = [{
+      id: 'user-1',
+      username: 'test',
+      email: 'test@no1hardy.ch',
+      firstname: 'Test',
+      lastname: 'User',
+      points: 100,
+      lastReviewedPoints: 10,
+      role: Role.USER,
+      applicationReviewedAt: new Date(),
+      userApplicationStatus: UserApplicationStatus.ACCEPTED
+    }]
+
+    service.getAllUsers().subscribe(users => {
+      expect(users).toEqual(mockUsers)
+    })
+
+    const req = httpMock.expectOne('/api/user/all')
+    expect(req.request.method).toBe('GET')
+    req.flush(mockUsers)
+  })
+
+  it('should return an empty list if no users are found', () => {
+    service.getAllUsers().subscribe(users => {
+      expect(users).toEqual([])
+    })
+
+    const req = httpMock.expectOne('/api/user/all')
+    expect(req.request.method).toBe('GET')
+    req.flush([])
   })
 })

6950e2f3b358c4c41650f64160adbbd8f5396438
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-06-20 20:01:00 +0200

    test: root navigation

--- frontend/src/app/root/root.component.spec.ts
+++ frontend/src/app/root/root.component.spec.ts
@@ -1,23 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 
-import { RootComponent } from './root.component'
+import {RootComponent} from './root.component'
+import {MockStore, provideMockStore} from '@ngrx/store/testing'
+import {User} from '../model/user/user'
+import {mockUser1} from '../model/mock/user.mock'
+import {selectUser} from '../user-management/store/user.feature'
 
 describe('RootComponent', () => {
   let component: RootComponent
   let fixture: ComponentFixture<RootComponent>
+  let store: MockStore
+  let mockUser: User
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RootComponent]
-    })
-    .compileComponents()
+      imports: [RootComponent],
+      providers: [provideMockStore()]
+    }).compileComponents()
 
     fixture = TestBed.createComponent(RootComponent)
+    mockUser = {...mockUser1}
+    store = TestBed.inject(MockStore)
     component = fixture.componentInstance
+    store.refreshState()
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should have user as null initially', () => {
+    store.overrideSelector(selectUser, null)
+    store.refreshState()
+    fixture.detectChanges()
+    expect(component.user()).toBeNull()
+  })
+
+  it('should have isLoggedIn as false initially', () => {
+    store.overrideSelector(selectUser, null)
+    store.refreshState()
+    fixture.detectChanges()
+    expect(component.isLoggedIn()).toBeFalse()
+  })
+
+  it('should update user when store emits a new user', () => {
+    store.overrideSelector(selectUser, mockUser)
+    store.refreshState()
+    fixture.detectChanges()
+    expect(component.user()).toEqual(mockUser)
+  })
+
+  it('should update isLoggedIn when user changes', () => {
+    store.overrideSelector(selectUser, mockUser)
+    store.refreshState()
+    fixture.detectChanges()
+    expect(component.isLoggedIn()).toBeTrue()
   })
 })

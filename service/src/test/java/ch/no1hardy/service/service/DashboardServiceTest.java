58737834cc6b13511cc67ca1bbd06f5b73d8d3d2
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-06-29 17:09:13 +0200

    test: dashboard response summary

--- service/src/test/java/ch/no1hardy/service/service/DashboardServiceTest.java
+++ service/src/test/java/ch/no1hardy/service/service/DashboardServiceTest.java
@@ -1,6 +1,7 @@
 package ch.no1hardy.service.service;
 
 import ch.no1hardy.service.front.dashboard.DashboardData;
+import ch.no1hardy.service.front.dashboard.UserSummary;
 import ch.no1hardy.service.front.leaderboard.RankingRes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,11 +42,21 @@ public class DashboardServiceTest {
                         .points(800)
                         .build()
         ));
+        when(userService.getUserSummary()).thenReturn(UserSummary.builder()
+                        .ranking(2)
+                        .isConfirmed(true)
+                        .points(1000)
+                        .percentage(70.0)
+                .build());
 
         DashboardData dashboardData = service.getDashboard();
 
         assertEquals("user-1", dashboardData.getLeaderboardPreview().getFirst().getId());
         assertEquals("user-2", dashboardData.getLeaderboardPreview().get(1).getId());
         assertEquals("user-3", dashboardData.getLeaderboardPreview().get(2).getId());
+        assertEquals(2, dashboardData.getUserSummary().getRanking());
+        assertEquals(1000, dashboardData.getUserSummary().getPoints());
+        assertEquals(70.0, dashboardData.getUserSummary().getPercentage());
+        assertEquals(true, dashboardData.getUserSummary().getIsConfirmed());
     }
 }

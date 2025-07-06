bd19fb81f7da60421f150ac20772831a36460261
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-07-06 13:14:07 +0200

    feature: close menu after navigation

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,7 +1,7 @@
-import {Component, computed, ElementRef, inject} from '@angular/core'
+import {Component, computed, ElementRef, inject, OnInit} from '@angular/core'
 import {NavItemComponent} from './components/nav-item/nav-item.component'
 import {UserMenuComponent} from './components/user-menu/user-menu.component'
-import {RouterLink} from '@angular/router'
+import {EventType, Router, RouterLink} from '@angular/router'
 import {UserButtonComponent} from './components/user-button/user-button.component'
 import {Store} from '@ngrx/store'
 import {selectIsAdmin, selectUser} from '../../../user-management/store/user.feature'
@@ -26,17 +26,27 @@ import {ViewportService} from '../../services/viewport/viewport.service'
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   private store = inject(Store)
   private el = inject(ElementRef)
   private viewportService = inject(ViewportService)
+  private router = inject(Router)
 
   user = this.store.selectSignal(selectUser)
   isAdmin = this.store.selectSignal(selectIsAdmin)
 
   isVpL = computed(() => !this.viewportService.isVPMorSmaller())
+
   hasNotifications = false
-  isExpanded = true
+  isExpanded = false
+
+  ngOnInit(): void {
+    this.router.events.subscribe(($event) => {
+      if ($event.type === EventType.NavigationEnd) {
+        this.closeMenu()
+      }
+    })
+  }
 
   logout(): void {
     this.store.dispatch(logout())
@@ -56,9 +66,7 @@ export class HeaderComponent {
     }
   }
 
-  closeMenuAfterTimeout(): void {
-    setTimeout(() => {
+  closeMenu(): void {
       this.isExpanded = false
-    }, 50)
   }
 }

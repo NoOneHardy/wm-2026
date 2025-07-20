a9f47d2a38a83e9b1bce1e5b43a0e330139aed4f
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-07-20 15:15:57 +0200

    feature: home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core'
+import {NgOptimizedImage} from '@angular/common'
+import {RouterLink} from '@angular/router'
+import {MatRipple} from '@angular/material/core'
 
 @Component({
   selector: 'wm-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink,
+    MatRipple
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

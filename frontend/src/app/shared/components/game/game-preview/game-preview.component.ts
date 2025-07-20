b5c9730ceea8fe02f882d52e6a8708b3848f506f
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-07-20 13:41:41 +0200

    feature: update game preview to support results

--- frontend/src/app/shared/components/game/game-preview/game-preview.component.ts
+++ frontend/src/app/shared/components/game/game-preview/game-preview.component.ts
@@ -3,6 +3,8 @@ import {BetGame} from '../../../../model/game/bet-game'
 import {DatePipe, NgIf, NgOptimizedImage} from '@angular/common'
 import {ButtonComponent} from '../../button/button.component'
 import {Router} from '@angular/router'
+import {PointService} from '../../../services/point/point.service'
+import {Bet} from '../../../../model/game/bet'
 
 @Component({
   selector: 'wm-game-preview',
@@ -18,14 +20,22 @@ import {Router} from '@angular/router'
 })
 export class GamePreviewComponent {
   private router = inject(Router)
+  private pointService = inject(PointService)
 
   game = input.required<BetGame>()
 
-  openBet: Signal<boolean> = computed(() => !this.game().bet)
+  openBet: Signal<boolean> = computed(() => !this.game().bet && !this.game().result)
+  points: Signal<number> = computed(() => {
+    return this.pointService.calculatePoints(this.game())
+  })
 
   focusGame(): void {
     const id = this.game().id
     const groupId = this.game().groupId
     this.router.navigateByUrl(`/bets/${groupId}/${id}`).then()
+  }
+
+  getJokerArray(bet: Bet): number[] {
+    return Array(bet.joker).map((_, i) => i + 1)
   }
 }

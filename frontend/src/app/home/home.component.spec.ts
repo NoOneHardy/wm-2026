22104ea9e09ca1f02b77986427d4068b92f6081b
Silas Noah Hardegger <silas.hardegger@outlook.com>
2025-07-20 15:23:00 +0200

    ci: fix tests

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 
-import { HomeComponent } from './home.component'
+import {HomeComponent} from './home.component'
+import {provideRouter} from '@angular/router'
 
 describe('HomeComponent', () => {
   let component: HomeComponent
@@ -8,9 +9,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents()
+      imports: [HomeComponent],
+      providers: [provideRouter([])]
+    }).compileComponents()
 
     fixture = TestBed.createComponent(HomeComponent)
     component = fixture.componentInstance

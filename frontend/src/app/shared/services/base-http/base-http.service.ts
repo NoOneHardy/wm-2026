c43f689a2b888604f4f71a4229d16d0179513e38
NoOneHardy <silas.hardegger@outlook.com>
2025-08-04 16:24:56 +0200

    feature: load notifications

--- frontend/src/app/shared/services/base-http/base-http.service.ts
+++ frontend/src/app/shared/services/base-http/base-http.service.ts
@@ -1,30 +1,41 @@
 import {inject, Injectable} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
-import {map, Observable} from 'rxjs'
+import {map, Observable, tap} from 'rxjs'
 import {BaseResponse} from './base-response'
 import {HttpOptions} from './options/http-options'
+import {Notification} from '../../../user-management/model/notification'
+import {Store} from '@ngrx/store'
+import {updateNotifications} from '../../../user-management/store/user.actions'
 
 @Injectable({
   providedIn: 'root'
 })
 export abstract class BaseHttpService {
   protected http = inject(HttpClient)
+  private store = inject(Store)
 
   protected get<T>(url: string, options?: HttpOptions): Observable<T> {
     return this.http.get<BaseResponse<T>>(url, options).pipe(
+      tap(r => this.updateNotifications(r.globalData.notifications)),
       map(r => r.data)
     )
   }
 
   protected post<T, D = object>(url: string, data: D, options?: HttpOptions): Observable<T> {
     return this.http.post<BaseResponse<T>>(url, data, options).pipe(
+      tap(r => this.updateNotifications(r.globalData.notifications)),
       map(r => r.data)
     )
   }
 
   protected put<T, D = object>(url: string, data: D, options?: HttpOptions): Observable<T> {
     return this.http.put<BaseResponse<T>>(url, data, options).pipe(
+      tap(r => this.updateNotifications(r.globalData.notifications)),
       map(r => r.data)
     )
+  }
+
+  private updateNotifications(notifications: Notification[]): void {
+    this.store.dispatch(updateNotifications({notifications}))
   }
 }

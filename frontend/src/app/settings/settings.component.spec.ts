67ad941fe32525dcf4d995cc0b2314fcf65fec4b
NoOneHardy <silas.hardegger@outlook.com>
2025-11-14 22:29:00 +0100

    test: fix settings test (#148)

--- frontend/src/app/settings/settings.component.spec.ts
+++ frontend/src/app/settings/settings.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 
-import { SettingsComponent } from './settings.component'
+import {SettingsComponent} from './settings.component'
+import {provideMockStore} from '@ngrx/store/testing'
+import {provideRouter} from '@angular/router'
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent
@@ -8,9 +10,9 @@ describe('SettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SettingsComponent]
-    })
-    .compileComponents()
+      imports: [SettingsComponent],
+      providers: [provideMockStore(), provideRouter([])]
+    }).compileComponents()
 
     fixture = TestBed.createComponent(SettingsComponent)
     component = fixture.componentInstance

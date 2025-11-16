66d40797440dc2869d2d25944eb8ad3b6b9238f6
NoOneHardy <silas.hardegger@outlook.com>
2025-11-16 17:35:09 +0100

    feature: basic account settings template (#150)

--- frontend/src/app/settings/account-settings/account-settings.component.spec.ts
+++ frontend/src/app/settings/account-settings/account-settings.component.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing'
 
 import {AccountSettingsComponent} from './account-settings.component'
 import {provideMockStore} from '@ngrx/store/testing'
+import {provideHttpClient} from '@angular/common/http'
 
 describe('AccountSettingsComponent', () => {
   let component: AccountSettingsComponent
@@ -10,7 +11,7 @@ describe('AccountSettingsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AccountSettingsComponent],
-      providers: [provideMockStore()]
+      providers: [provideMockStore(), provideHttpClient()]
     }).compileComponents()
 
     fixture = TestBed.createComponent(AccountSettingsComponent)

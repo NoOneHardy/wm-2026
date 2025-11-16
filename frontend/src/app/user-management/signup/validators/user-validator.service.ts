952c0d09a50b0d6b660cbd013b22e2eb2b38bf1c
NoOneHardy <silas.hardegger@outlook.com>
2025-11-16 17:29:41 +0100

    feature: fix infinitely pending async validators (#150)

--- frontend/src/app/user-management/signup/validators/user-validator.service.ts
+++ frontend/src/app/user-management/signup/validators/user-validator.service.ts
@@ -1,19 +1,26 @@
 import {inject, Injectable} from '@angular/core'
 import {UserService} from '../../user.service'
 import {AbstractControl, AsyncValidatorFn} from '@angular/forms'
-import {debounceTime, distinctUntilChanged, first, map, switchMap} from 'rxjs'
+import {first, map, of, switchMap, timer} from 'rxjs'
+import {Store} from '@ngrx/store'
+import {selectUser} from '../../store/user.feature'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserValidatorService {
   private userService = inject(UserService)
+  private store = inject(Store)
+  private currentUser = this.store.selectSignal(selectUser)
 
   usernameAvailable(): AsyncValidatorFn {
     return (control: AbstractControl) => {
-      return control.valueChanges.pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
+      const currentUsername = this.currentUser()?.username
+      const inputUsername = control.value
+
+      if (currentUsername === inputUsername) return of(null)
+      return timer(500).pipe(
+        map(() => inputUsername),
         switchMap(v => this.userService.checkUsername(v)),
         map(check => {
           if (check.isUsernameAvailable) return null
@@ -26,10 +33,13 @@ export class UserValidatorService {
 
   emailAvailable(): AsyncValidatorFn {
     return (control: AbstractControl) => {
-      return control.valueChanges.pipe(
-        debounceTime(500),
-        distinctUntilChanged(),
-        switchMap(v => this.userService.checkEmail(v)),
+      const currentEmail = this.currentUser()?.email
+      const inputEmail = control.value
+
+      if (currentEmail === inputEmail) return of(null)
+      return timer(500).pipe(
+        map(() => inputEmail),
+        switchMap((v) => this.userService.checkEmail(v)),
         map(check => {
           if (check.isEmailAvailable) return null
           return {emailAvailable: true}

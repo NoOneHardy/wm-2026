29e914a1406cab7c605b664295f3a1aef35c2153
NoOneHardy <silas.hardegger@outlook.com>
2025-11-16 17:35:09 +0100

    feature: basic account settings template (#150)

--- frontend/src/app/settings/account-settings/account-settings.component.ts
+++ frontend/src/app/settings/account-settings/account-settings.component.ts
@@ -1,30 +1,52 @@
 import {Component, inject, Signal} from '@angular/core'
 import {AvatarUploadComponent} from '../../shared/components/avatar-upload/avatar-upload.component'
 import {Store} from '@ngrx/store'
-import {selectUser} from '../../user-management/store/user.feature'
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import {selectIsUserLoading, selectUser} from '../../user-management/store/user.feature'
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
 import {User} from '../../model/user/user'
+import {FormFieldComponent} from '../../shared/components/form-field/form-field.component'
+import {ButtonComponent} from '../../shared/components/button/button.component'
+import {UserValidatorService} from '../../user-management/signup/validators/user-validator.service'
+import {NgIf} from '@angular/common'
+import {hasError} from '../../shared/helper/form-field-error'
 
 @Component({
   selector: 'wm-account-settings',
   standalone: true,
   imports: [
     AvatarUploadComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormFieldComponent,
+    ButtonComponent,
+    NgIf
   ],
   templateUrl: './account-settings.component.html',
   styleUrl: './account-settings.component.css'
 })
 export class AccountSettingsComponent {
   private store = inject(Store)
+  private userValidatorService = inject(UserValidatorService)
 
   user: Signal<User | null> = this.store.selectSignal(selectUser)
+  isUserLoading: Signal<boolean> = this.store.selectSignal(selectIsUserLoading)
 
   formGroup = new FormGroup({
-    avatar: new FormControl<File | null>(null)
+    avatar: new FormControl<File | null>(null),
+    username: new FormControl<string>(this.user()?.username ?? '', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(5)],
+      asyncValidators: [this.userValidatorService.usernameAvailable()]
+    }),
+    email: new FormControl<string>(this.user()?.email ?? '', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email, this.userValidatorService.emailAvailable],
+    })
   })
 
-  log(): void {
-    console.log(this.formGroup.value)
+  save(): void {
+    this.formGroup.markAllAsTouched()
+    if (this.formGroup.invalid) return
   }
+
+  protected readonly hasError = hasError
 }

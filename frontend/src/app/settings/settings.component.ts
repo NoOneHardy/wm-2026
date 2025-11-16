0a92287b71174c5dd8c0ffe19e9f503ae269df3e
NoOneHardy <silas.hardegger@outlook.com>
2025-11-16 14:55:35 +0100

    refactor: moved side nav item to shared (#255)

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core'
 import {SideNavComponent} from '../shared/components/side-nav/side-nav.component'
-import {SideNavItemComponent} from '../side-nav-item/side-nav-item.component'
+import {SideNavItemComponent} from '../shared/components/side-nav-item/side-nav-item.component'
 import {Store} from '@ngrx/store'
 import {logout} from '../user-management/store/user.actions'
 import {RouterOutlet} from '@angular/router'

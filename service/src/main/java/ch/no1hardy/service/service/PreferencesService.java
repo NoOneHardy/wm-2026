ca161791ede8af465c5944f90a2b0f8eaa449889
NoOneHardy <silas.hardegger@outlook.com>
2025-11-22 13:04:55 +0100

    documentation: javadoc (#265)

--- service/src/main/java/ch/no1hardy/service/service/PreferencesService.java
+++ service/src/main/java/ch/no1hardy/service/service/PreferencesService.java
@@ -28,18 +28,41 @@ public class PreferencesService {
     private final NotificationPreferenceRepository notificationPreferenceRepository;
     private final PreferenceKeyProvider preferenceKeyProvider;
 
+    /**
+     * Get notification preferences for the logged-in user as DTOs
+     *
+     * @return a list of notification preference DTOs wrapped in an Optional
+     */
     public Optional<List<NotificationPreferenceRes>> getNotificationPrefs() {
         return getNotificationPrefsRaw().map(mapper::toDto);
     }
 
+    /**
+     * Get notification preferences for the logged-in user
+     *
+     * @return a list of notification preferences wrapped in an Optional
+     */
     public Optional<List<NotificationPreference>> getNotificationPrefsRaw() {
         return authService.getLoggedInUser().map(this::getNotificationPrefsRaw);
     }
 
+    /**
+     * Get notification preferences for a specific user
+     *
+     * @param user the user whose preferences are to be retrieved
+     * @return the user's notification preferences
+     */
     public List<NotificationPreference> getNotificationPrefsRaw(@NotNull User user) {
         return ensureNotificationPrefs(user);
     }
 
+    /**
+     * Ensure that all notification preferences exist for the given user.
+     * If any preferences are missing, they will be created with default values.
+     *
+     * @param user the user for whom to ensure notification preferences
+     * @return the complete list of notification preferences for the user
+     */
     public List<NotificationPreference> ensureNotificationPrefs(@NotNull User user) {
         List<NotificationPreferenceKey> keys = preferenceKeyProvider.getNotificationPreferenceKeys();
 
@@ -68,6 +91,14 @@ public class PreferencesService {
         return prefs;
     }
 
+    /**
+     * Check if a specific notification type is disabled for a user on a given channel
+     *
+     * @param user    the user to check
+     * @param channel the notification channel
+     * @param type    the notification type
+     * @return true if the notification is disabled, false otherwise
+     */
     public boolean hasDisabledNotification(@NotNull User user, @NotNull Channel channel, @NotNull NotificationType type) {
         return getNotificationPrefsRaw(user).stream()
                 .filter(pref -> pref.getChannel().equals(channel) && pref.getType().equals(type))
@@ -76,6 +107,12 @@ public class PreferencesService {
                 .orElse(false); // fallback to false if no preference is found
     }
 
+    /**
+     * Update notification preferences for the logged-in user
+     *
+     * @param prefs the list of notification preference requests to update
+     * @return the updated list of notification preference DTOs wrapped in an Optional
+     */
     public Optional<List<NotificationPreferenceRes>> updateNotificationPrefs(List<NotificationPreferenceReq> prefs) {
         getNotificationPrefsRaw().ifPresent(existingPrefs ->
                 prefs.forEach(req -> existingPrefs.stream()    // loop over incoming prefs

3192790e03bd93584ca2f376971cabd8ec1a9fd1
NoOneHardy <silas.hardegger@outlook.com>
2025-12-29 14:59:24 +0100

    test: email verification tests (#45)

--- frontend/src/app/email-verification/email-verification.component.spec.ts
+++ frontend/src/app/email-verification/email-verification.component.spec.ts
@@ -1,23 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { EmailVerificationComponent } from './email-verification.component';
+import {provideMockStore} from '@ngrx/store/testing'
+import {ComponentFixture, TestBed} from '@angular/core/testing'
+import {EmailVerificationComponent} from './email-verification.component'
+import {verifyEmail} from '../user-management/store/user.actions'
+import {ActivatedRoute} from '@angular/router'
 
 describe('EmailVerificationComponent', () => {
-  let component: EmailVerificationComponent;
-  let fixture: ComponentFixture<EmailVerificationComponent>;
+  let component: EmailVerificationComponent
+  let fixture: ComponentFixture<EmailVerificationComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EmailVerificationComponent]
-    })
-    .compileComponents();
+      imports: [EmailVerificationComponent],
+      providers: [provideMockStore(), {
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            queryParamMap: {
+              get: () => {
+                return 'test-code'
+              }
+            }
+          }
+        }
+      }]
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(EmailVerificationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(EmailVerificationComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should read code from query params', () => {
+    const spy = spyOn(component['store'], 'dispatch')
+    component.ngOnInit()
+    expect(spy).toHaveBeenCalledOnceWith(verifyEmail({code: 'test-code'}))
+  })
+})
--- frontend/src/app/email-verification/email-verification.component.ts
+++ frontend/src/app/email-verification/email-verification.component.ts
@@ -2,6 +2,7 @@ import {Component, inject, OnInit} from '@angular/core'
 import {SpinnerComponent} from '../shared/components/spinner/spinner.component'
 import {Store} from '@ngrx/store'
 import {verifyEmail} from '../user-management/store/user.actions'
+import {ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'wm-email-verification',
@@ -14,9 +15,10 @@ import {verifyEmail} from '../user-management/store/user.actions'
 })
 export class EmailVerificationComponent implements OnInit {
   private store = inject(Store)
+  private activatedRoute = inject(ActivatedRoute)
 
   ngOnInit(): void {
-    const urlParams = new URLSearchParams(window.location.search)
+    const urlParams = this.activatedRoute.snapshot.queryParamMap
     const code = urlParams.get('code')
     if (code) {
       this.store.dispatch(verifyEmail({code}))

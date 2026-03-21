1f5d17a6050b2b3009f84ebe135b0741354eab77
NoOneHardy <silas.hardegger@outlook.com>
2026-03-21 08:10:04 +0100

    bugfix: clear jwt cookie if user is not found (#300)

--- service/src/main/java/ch/no1hardy/service/exception/user/UsernameNotFoundException.java
+++ service/src/main/java/ch/no1hardy/service/exception/user/UsernameNotFoundException.java
@@ -1,14 +1,13 @@
 package ch.no1hardy.service.exception.user;
 
-import ch.no1hardy.service.common.StringHelper;
 import ch.no1hardy.service.exception.NotFoundException;
 import jakarta.validation.constraints.NotNull;
 
 public class UsernameNotFoundException extends NotFoundException {
-    public UsernameNotFoundException(@NotNull String id) {
+    public UsernameNotFoundException(@NotNull String username) {
         super(
-                "User '" + id + "' not found.",
-                "Benutzer '" + StringHelper.getFormattedId(id) + "' nicht gefunden."
+                "User '" + username + "' not found.",
+                "Benutzer '" + username + "' nicht gefunden."
         );
     }
 }

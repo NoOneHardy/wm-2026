e4042597f8eb4556bcda95237eebd1df70b4f39c
NoOneHardy <silas.hardegger@outlook.com>
2026-03-29 17:20:15 +0200

    feature: extract payout board (#304)

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {NgOptimizedImage} from '@angular/common'
 import {RouterLink} from '@angular/router'
 import {MatRipple} from '@angular/material/core'
 import {InfoCardComponent} from './components/info-card/info-card.component'
+import {PayoutBoardComponent} from './components/payout-board/payout-board.component'
 
 @Component({
   selector: 'wm-home',
@@ -11,7 +12,8 @@ import {InfoCardComponent} from './components/info-card/info-card.component'
     NgOptimizedImage,
     RouterLink,
     MatRipple,
-    InfoCardComponent
+    InfoCardComponent,
+    PayoutBoardComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'

399b351f19a7bbcfdcc87190dba7101bcd876bfe
NoOneHardy <silas.hardegger@outlook.com>
2026-03-29 20:57:16 +0200

    bugfix: fix style overwrites (#304)

--- frontend/src/app/home/components/stadium/stadium.component.ts
+++ frontend/src/app/home/components/stadium/stadium.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core'
 @Component({
   selector: 'wm-stadium',
   templateUrl: './stadium.component.html',
-  styleUrls: ['./stadium.component.css'],
+  styleUrl: './stadium.component.css',
   standalone: true
 })
 export class StadiumComponent {

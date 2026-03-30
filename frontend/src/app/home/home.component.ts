108e96fc830bf881b2c572d410a1f7efac3a2d84
NoOneHardy <silas.hardegger@outlook.com>
2026-03-30 21:25:07 +0200

    feature: show data (#287)

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,7 +1,10 @@
-import {Component} from '@angular/core'
+import {Component, computed, inject, OnInit} from '@angular/core'
+import {Store} from '@ngrx/store'
 import {InfoCardComponent} from './components/info-card/info-card.component'
 import {PayoutBoardComponent} from './components/payout-board/payout-board.component'
 import {HeroSectionComponent} from './components/hero-section/hero-section.component'
+import {loadHomeData} from './store/home.actions'
+import {selectHomeData} from './store/home.feature'
 
 @Component({
   selector: 'wm-home',
@@ -14,6 +17,20 @@ import {HeroSectionComponent} from './components/hero-section/hero-section.compo
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private store = inject(Store)
 
+  homeData = this.store.selectSignal(selectHomeData)
+  stats = computed(() => {
+    const data = this.homeData()
+    return {
+      jackpot: data ? `CHF ${data.jackpot}` : 'CHF --',
+      players: data ? `${data.players}` : '--',
+      games: data ? `${data.games}` : '--'
+    }
+  })
+
+  ngOnInit(): void {
+    this.store.dispatch(loadHomeData())
+  }
 }

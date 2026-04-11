ace48ce6ac31d9a55b8fce661b2a6130d67dba22
NoOneHardy <silas.hardegger@outlook.com>
2026-04-11 14:14:30 +0200

    feature: show quick filter for status (#229)

--- frontend/src/app/admin/user-management/user-management.component.ts
+++ frontend/src/app/admin/user-management/user-management.component.ts
@@ -1,20 +1,28 @@
 import {Component, computed, inject, OnInit, Signal} from '@angular/core'
 import {Store} from '@ngrx/store'
 import {selectIsAdminLoading, selectUsers} from '../store/admin.feature'
-import { NgOptimizedImage } from '@angular/common'
+import {NgOptimizedImage} from '@angular/common'
 import {confirmUser, denyUser, loadUsers} from '../store/admin.actions'
 import {User} from '../../model/user/user'
 import {UserApplicationStatus} from '../../model/user/user-application-status'
 import {selectUser} from '../../user-management/store/user.feature'
 import {MatRipple} from '@angular/material/core'
 import {SpinnerComponent} from '../../shared/components/spinner/spinner.component'
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle'
+import {MatIcon} from '@angular/material/icon'
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import {toSignal} from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'wm-user-management',
   imports: [
     NgOptimizedImage,
     MatRipple,
-    SpinnerComponent
+    SpinnerComponent,
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    MatIcon,
+    ReactiveFormsModule
   ],
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.css'
@@ -26,9 +34,20 @@ export class UserManagementComponent implements OnInit {
   currentUser: Signal<User | null> = this.store.selectSignal(selectUser)
   isLoading: Signal<boolean> = this.store.selectSignal(selectIsAdminLoading)
 
-  get users(): Signal<User[]> {
-    return computed(() => {
-      return this._users().filter(user => user.id !== this.currentUser()?.id).sort((a, b) => {
+  formGroup = new FormGroup({
+    quickFilter: new FormControl<UserApplicationStatus[]>([UserApplicationStatus.PENDING])
+  })
+
+  filters = toSignal(this.formGroup.valueChanges)
+
+  users = computed(() => {
+    const filters = this.filters()
+    const quickFilter = filters?.quickFilter ?? this.formGroup.controls.quickFilter.getRawValue() ?? []
+
+    return this._users()
+      .filter(user => user.id !== this.currentUser()?.id)
+      .filter(user => quickFilter.length === 0 || quickFilter.includes(user.userApplicationStatus))
+      .sort((a, b) => {
         if (a.userApplicationStatus === b.userApplicationStatus) return a.username.localeCompare(b.username)
 
         if (this.isDenied(a) && this.isAccepted(b)) return -1
@@ -37,8 +56,7 @@ export class UserManagementComponent implements OnInit {
         if (this.isAccepted(a) || this.isDenied(a)) return 1
         return -1
       })
-    })
-  }
+  })
 
   ngOnInit(): void {
     this.store.dispatch(loadUsers())
@@ -59,4 +77,6 @@ export class UserManagementComponent implements OnInit {
   deny(id: string): void {
     this.store.dispatch(denyUser({id}))
   }
+
+  protected readonly UserApplicationStatus = UserApplicationStatus
 }

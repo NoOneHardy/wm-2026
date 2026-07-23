25fd3a6041a87c440655baeb7e9a6f7226fb0b85
NoOneHardy <silas.hardegger@outlook.com>
2026-07-23 23:19:33 +0200

    📦 deps: adapt code to Spring Boot 4 and Jackson 3 APIs (#353)

--- service/src/test/java/ch/no1hardy/service/config/ApiResponseAdviceTest.java
+++ service/src/test/java/ch/no1hardy/service/config/ApiResponseAdviceTest.java
@@ -2,8 +2,8 @@ package ch.no1hardy.service.config;
 
 import ch.no1hardy.service.controller.TeamController;
 import ch.no1hardy.service.front.GlobalData;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.MethodParameter;

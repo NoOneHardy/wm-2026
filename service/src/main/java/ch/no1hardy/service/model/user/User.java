184ff4f4bc48a44a122263b5dfe798fe4acb0381
NoOneHardy <silas.hardegger@outlook.com>
2026-07-27 22:36:38 +0200

    ♻️ rename wm-prefixed database table names to beticon (#435)

--- service/src/main/java/ch/no1hardy/service/model/user/User.java
+++ service/src/main/java/ch/no1hardy/service/model/user/User.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 
 @Data
 @Entity
-@Table(name = "wm-user")
+@Table(name = "beticon-user")
 @EqualsAndHashCode(callSuper = false)
 public class User extends BaseEntity implements UserDetails {
     /**
